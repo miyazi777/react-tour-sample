c87457c1214ad4f83dc4b61d5805dbb08fd96ac6
miyazi777 <m71203@gmail.com>
2021-05-12 19:31:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Tour from 'reactour';
 
 function App() {
+
+  const [isOpen, setOpen] = useState(false);
+
+  const handleStart = () => {
+    setOpen(!isOpen);
+    console.log('handle start', isOpen);
+  }
+
+  const steps = [
+    {
+      selector: '[data-tut="reactour__step1"]',
+      content: 'step1',
+    },
+    {
+      selector: '[data-tut="reactour__step2"]',
+      content: 'step2',
+    },
+    {
+      selector: '[data-tut="reactour__step3"]',
+      content: 'step3',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>react tour test</p>
+      <button onClick={handleStart}>start</button>
+      <div data-tut="reactour__step1" style={{marginTop: '50px'}}>test1</div>
+      <div data-tut="reactour__step2" style={{marginTop: '50px'}}>test2</div>
+      <div data-tut="reactour__step3" style={{marginTop: '50px'}}>test3</div>
+      <Tour
+        steps={steps}
+        isOpen={isOpen}
+        onRequestClose={() => setOpen(false)}
+      />
     </div>
   );
 }
